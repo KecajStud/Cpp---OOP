a074352e0e15f5eb5858773c9dee0256e5e7f00f
Jacek Grzegrzolka <jacekgrzegrzolka@gmail.com>
2022-02-23 23:16:29 +0100

    Implementataion of struct Transaction

--- interface.h
+++ interface.h
@@ -10,5 +10,33 @@
 #include <map>
 #include <time.h>
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+///////////// Interface
+
+///////////// Class announcements
+
+class Buyer;
+class Seller;
+class BankOfUniverse;
+
+///////////// Transaction
+
+enum class TransactionStatus{
+    registered, finalized
+}
+const std::string statusName[] = {"registered", "finalized"};
+struct Transaction{
+    int id;
+    Buyer *toWhom;
+    Seller *fromWho;
+    int cnt;
+    int itemPrice;
+    TransactionStatus status;
+
+    //constructor and copy constructor:
+    Transaction(int id, Buyer *toWhom, Seller *fromWhom, int cnt, int itemPrice, int TransactionStatus status);
+    Transaction(const Transaction&);
+};
+
 
 #endif //DILITHIUM_INTERFACE_H
